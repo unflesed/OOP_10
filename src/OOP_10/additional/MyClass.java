75931660eeb4a6e2b023357a0efea40b3d19b7a6
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-10-29 21:49:30 +0300

    additional

--- src/OOP_10/additional/MyClass.java
+++ src/OOP_10/additional/MyClass.java
@@ -1,7 +1,12 @@
 package OOP_10.additional;
 
 public class MyClass<T>  {
-    /*public static <T> factoryMethod(){
-        return new T();
-    }*/
+    public static <T> void factoryMethod(T t){
+        System.out.println(t.toString());
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        MyClass.factoryMethod(new Integer(2));
+    }
 }
